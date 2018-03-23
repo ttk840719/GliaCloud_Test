a0c8961d69e398dc40b2f4ba5e81fa10af00bdf2
Ting <ttk840719@gmail.com>
2018-03-23 04:59:47 -0400

    q2 up

--- Part2/q4.py
+++ Part2/q4.py
@@ -7,15 +7,15 @@ def getPage(url):
     res = requests.get(url).text
     ##print(res.text)
     soup =  BeautifulSoup(res,"html.parser")
-
+    return soup
 
 def getInfo(soup):
     contents = soup.select('.r-ent')
     for i in contents:
         dct = {}
         dct["date"] = i.select('div.date')[0].text
-        dct["title"] = each_item.select('div.title')[0].text
-        dct["author"] = each_item.select('div.author')[0].text
+        dct["title"] = i.select('div.title')[0].text
+        dct["author"] = i.select('div.author')[0].text
 
         #get the content base on href
 getPage("https://www.ptt.cc/bbs/movie/index.html")
